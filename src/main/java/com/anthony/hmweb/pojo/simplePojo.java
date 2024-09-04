a342b02f400a5760536ceac682425b274e687a25
Anthony <w3113545186@163.com>
2024-09-04 15:05:24 +0800

    Init this project

--- src/main/java/com/anthony/hmweb/pojo/simplePojo.java
+++ src/main/java/com/anthony/hmweb/pojo/simplePojo.java
@@ -1,2 +1,28 @@
-package com.anthony.hmweb.pojo;public class simplePojo {
+package com.anthony.hmweb.pojo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class simplePojo {
+
+    public class Address {
+        private String province;
+        private String city;
+    }
+
+    public class User {
+        private String name;
+        private Integer age;
+        private Address address;
+
+    }
+
+    @RequestMapping("/ComplexPojo")
+    public String ComplexPojo(User user){
+        System.out.println(user);
+        return "OK";
+    }
+
+
 }
